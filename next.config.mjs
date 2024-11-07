ff70af393f988a43992487eb82ad86721c43c2e5
SumitXYZ098 <sumit@thexyzstudio.com>
2024-11-07 15:57:40 +0530

    fix bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['universalapp.xyz.studio'], // Add your external domain here
-    
-      },
+  images: {
+    domains: ["universalapp.xyz.studio"], // Add your external domain here
+  },
+  distDir: "build",
 };
 
 export default nextConfig;
